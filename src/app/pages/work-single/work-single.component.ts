65b54b266bd10617aef56465526d1609fb12d3ac
oihoihoih <ochyuo@hotmail.com>
2021-04-08 22:02:36 +0200

    Volver button in Single Page first approach

--- src/app/pages/work-single/work-single.component.ts
+++ src/app/pages/work-single/work-single.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Works } from 'src/app/interfaces/works.interface';
 import { WorkService } from 'src/app/services/work.service';
 
@@ -16,13 +16,18 @@ export class WorkSingleComponent implements OnInit {
   singleWork!: Works;
 
   constructor( private activatedRoute: ActivatedRoute,
-               private workService: WorkService ) { }
+               private workService: WorkService,
+               private router: Router ) { }
 
   ngOnInit(): void {
     this.activatedRoute.params
       .subscribe( ({ listName }) => this.listName = listName );
     
     this.singleWork = this.workService.getSingleWork( this.listName );  
+   }
+
+   volver() {
+    this.router.navigate(['/work']);
    }
 
 }
