cba56e1996016368bd62757b05a9efffab961092
oihoihoih <ochyuo@hotmail.com>
2021-04-14 23:01:26 +0200

    work-list image reveal animation with css

--- src/app/pages/work/work.component.ts
+++ src/app/pages/work/work.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { WorkService } from 'src/app/services/work.service';
 import { Works } from '../../interfaces/works.interface';
+
 
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
-  styleUrls: ['./work.component.scss']
+  styleUrls: ['./work.component.scss'],
 })
 export class WorkComponent implements OnInit {
+
 
   works: Works[] = [];
 
   constructor( private workService: WorkService ) { }
 
   ngOnInit(): void {
-    this.works = this.workService.getWorks();
+    this.works = this.workService.getWorks(); 
   }
+
+
 
 }
